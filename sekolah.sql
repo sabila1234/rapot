86902a8a4889789cefc3afcaa93e076649d9c9e0
aufaa03 <142073902+aufaa03@users.noreply.github.com>
2024-10-15 11:15:52 +0700

    Add files via upload

---
+++ sekolah.sql
@@ -0,0 +1,200 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 15, 2024 at 06:15 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sekolah`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `username`, `password`) VALUES
+(1, 'admin1', 'admin1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_guru`
+--
+
+CREATE TABLE `tb_guru` (
+  `id_guru` int(11) NOT NULL,
+  `nip` varchar(30) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` varchar(128) NOT NULL,
+  `telpon` varchar(20) NOT NULL,
+  `agama` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_guru`
+--
+
+INSERT INTO `tb_guru` (`id_guru`, `nip`, `nama`, `alamat`, `telpon`, `agama`) VALUES
+(1, '222', 'aufa', 'Desa Pelutan', '0789678', 'islam');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_nilai_ujian`
+--
+
+CREATE TABLE `tb_nilai_ujian` (
+  `id` int(11) NOT NULL,
+  `no_ujian` char(10) NOT NULL,
+  `pelajaran` varchar(100) NOT NULL,
+  `nilai_ujian` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pelajaran`
+--
+
+CREATE TABLE `tb_pelajaran` (
+  `id` int(11) NOT NULL,
+  `pelajaran` varchar(50) NOT NULL,
+  `jurusan` varchar(50) NOT NULL,
+  `guru` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_siswa`
+--
+
+CREATE TABLE `tb_siswa` (
+  `nisn` char(6) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` varchar(256) NOT NULL,
+  `kelas` varchar(6) NOT NULL,
+  `jurusan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_siswa`
+--
+
+INSERT INTO `tb_siswa` (`nisn`, `nama`, `alamat`, `kelas`, `jurusan`) VALUES
+('96878', 'aufaa', 'Desa Gondang', 'XI ', 'PPLG');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_ujian`
+--
+
+CREATE TABLE `tb_ujian` (
+  `no_ujian` char(10) NOT NULL,
+  `tgl_ujian` date NOT NULL,
+  `nisn` char(10) NOT NULL,
+  `jurusan` varchar(50) NOT NULL,
+  `total_nilai` int(11) NOT NULL,
+  `nilai_tertinggi` int(11) NOT NULL,
+  `nilai_rata2` int(11) NOT NULL,
+  `hasil_ujian` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `tb_guru`
+--
+ALTER TABLE `tb_guru`
+  ADD PRIMARY KEY (`id_guru`);
+
+--
+-- Indexes for table `tb_nilai_ujian`
+--
+ALTER TABLE `tb_nilai_ujian`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_pelajaran`
+--
+ALTER TABLE `tb_pelajaran`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_siswa`
+--
+ALTER TABLE `tb_siswa`
+  ADD PRIMARY KEY (`nisn`);
+
+--
+-- Indexes for table `tb_ujian`
+--
+ALTER TABLE `tb_ujian`
+  ADD PRIMARY KEY (`no_ujian`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tb_guru`
+--
+ALTER TABLE `tb_guru`
+  MODIFY `id_guru` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tb_nilai_ujian`
+--
+ALTER TABLE `tb_nilai_ujian`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_pelajaran`
+--
+ALTER TABLE `tb_pelajaran`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
