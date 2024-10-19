655325dfcfd7541fa7fdcc21de25a37ac90944cb
aufaa03 <142073902+aufaa03@users.noreply.github.com>
2024-10-19 08:52:58 +0700

    Delete rapot-main directory

--- rapot-main/admin/function/hapus-guru.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$id = $_GET['id'];
-$cek_nip = "SELECT * FROM tb_guru WHERE nip='$id'";
-$hasil_nip = $koneksi->query($cek_nip);
-$sql = "DELETE FROM tb_murid"
-
-?>
--- rapot-main/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$koneksi = new mysqli("localhost", "root", "", "sekolah");
-// if ($koneksi) {
-//     echo"koneksi berhasil";
-// } else {
-//     echo"koneksi gagal";
-//     # code...
-// }
-?>
